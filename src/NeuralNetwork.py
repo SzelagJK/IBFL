fbe746e53794e481ed57fafdbb9bed1ddf19f043
Jakub Szelag <jakub.szelag@students.plymouth.ac.uk>
2025-09-01 22:16:33 +0100

    Removed redundant comments from neural network file

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -2,20 +2,17 @@ import torch
 import torch.nn as nn
 
 class NeuralNetwork(nn.Module):
-	def __init__(self, input_size): # Defines components of the neural network
+	def __init__(self, input_size): 
 		super(NeuralNetwork, self).__init__()
-		# Fully connected layers
 		self.fc1 = nn.Linear(input_size, 8)
 		self.fc2 = nn.Linear(8, 16)
 		self.fc3 = nn.Linear(16, 8)
 		self.fc4 = nn.Linear(8, 1)
-		# Activation function
 		self.sigmoid = nn.Sigmoid()
-		# Dropouts
 		self.dropoutMiddle = nn.Dropout(p=0.5)
 		self.dropoutEnd = nn.Dropout(p=0.2)
 
-	def forward(self, x): # Specifies the flow of the neural network
+	def forward(self, x): 
 		x = self.sigmoid(self.fc1(x))
 		x = self.dropoutMiddle(x)
 		x = self.sigmoid(self.fc2(x))
